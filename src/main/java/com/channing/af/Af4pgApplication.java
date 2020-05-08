ff28d9709b15d3a9a86846bf0d453068d7078965
channing <chenglin.wang@kiongroup.com>
2020-05-08 21:05:37 +0800

    project init

---
+++ src/main/java/com/channing/af/Af4pgApplication.java
@@ -0,0 +1,13 @@
+package com.channing.af;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Af4pgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Af4pgApplication.class, args);
+	}
+
+}
